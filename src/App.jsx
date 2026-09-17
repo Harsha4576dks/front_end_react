99fc80b00428ff7f32d9781289ff250e98fadf47
Harsha4576dks <harshivahm@gmail.com>
2026-09-17 18:23:24 +0530

    exmaple

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
 import './App.css'
+import MovieCard from "./components/MovieCard"
 
 function App() {
     return (
-      <>
-      <Text />
-      <Text />
+      /*  <>
+      <Text  display="hi"/>
+      <Text display="max" />
       </>
   );
 }
 
-function Text(){
+function Text({display}){
 return (
   <div>
-    <p>hello </p>
+    <p>{display} </p>
   </div>
+  */
+    <>
+  <MovieCard movie={{title: "Tim's Film", release_date:"2024"}}/>
+  
+  </>
 );
 }
 

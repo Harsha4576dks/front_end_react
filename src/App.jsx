c6735288c2045c886f6ad44adc81b36f6a74cc7f
Harsha4576dks <harshivahm@gmail.com>
2026-09-17 17:26:10 +0530

    basics

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+    return (
+      <>
+      <Text />
+      <Text />
+      </>
+  );
+}
+
+function Text(){
+return (
+  <div>
+    <p>hello </p>
+  </div>
+);
+}
+
+export default App

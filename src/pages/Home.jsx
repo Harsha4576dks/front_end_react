ec5c3f9dd40bb4568247010819197d4dfbc506ed
Harsha4576dks <harshivahm@gmail.com>
2026-09-19 14:48:52 +0530

    navigation router

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,37 @@
 import MovieCard from "../components/MovieCard";
+import { useState } from "react";
 
-function home(){
+function Home(){
+    const [searchQuery, setSearchQuery] = useState("");
+
     const movies =[
-        {id:1, name:"om", release_date:"2000"},
-        {id:2, name:"saho", release_date:"2020"},
-        {id:3, name:"kgf", release_date:"2023"},
+        {id:1, title:"om", release_date:"2000"},
+        {id:2, title:"saho", release_date:"2020"},
+        {id:3, title:"kgf", release_date:"2023"},
     ];
+    const handlesearch = (e) => {
+        e.preventDefault()
+        alert(searchQuery)
+        setSearchQuery("")
+    };
 
     return (
         <div className="home">
+            <form onSubmit={handlesearch} className="search-form">
+                <input type="text"
+                placeholder="search for movies"
+                className="search-input"
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+                />
+                <button type="submit" className="search-button">search</button>
+                </form>
 
         <div className="movies-grid">{
-            movies.map((movie)=>(
-            <MovieCard movie={movie} key={movie.id}/>
+            movies.map(
+                (movie)=> 
+                    movie.title.toLowerCase().startsWith(searchQuery.toLowerCase()) &&
+                    (<MovieCard movie={movie} key={movie.id}/>
             )) 
             }</div>
         </div>
@@ -20,4 +39,4 @@ function home(){
     );
 }
 
-export default home
+export default Home;
---
+++ src/pages/favourites.jsx
@@ -0,0 +1,8 @@
+function Favourites(){
+    return <div className="favourite-empty">
+        <h2>No Favourite Movies yet</h2>
+        <p>start adding movie to your favourites  </p>
+        </div>
+}
+
+export default Favourites

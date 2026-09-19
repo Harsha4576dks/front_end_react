28fdcfd70a68650abc80ab8410204c8da17a6b22
Harsha4576dks <harshivahm@gmail.com>
2026-09-19 15:25:34 +0530

    css

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
-import './App.css'
+import './css/App.css';
 import Favourites from './pages/favourites';
 import Home from './pages/Home'
 import {Routes, Route} from "react-router-dom"
+import NavBar from './components/NavBar';
 
 function App() {
     return (
+        <div>
+            <NavBar/>
         <main className="main-content">
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/favourites" element={<Favourites/>}/>
             </Routes>
         </main>
+        </div>
     );
 }
 
--- src/pages/favourites.jsx
+++ src/pages/favourites.jsx
@@ -1,3 +1,4 @@
+import "../css/Favourite.css"
 function Favourites(){
     return <div className="favourite-empty">
         <h2>No Favourite Movies yet</h2>
